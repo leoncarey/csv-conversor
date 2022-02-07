0d935007b16945a2b37e6f99325e27a07151ffa9
Leonardo Carey <careycoder@gmail.com>
2022-02-07 17:08:11 -0300

    Updates

--- src/renderer/controllers/fileDownloader.ts
+++ src/renderer/controllers/fileDownloader.ts
@@ -1,13 +1,24 @@
-import fs from 'fs';
-
 class FileDownloader {
   static async readFile(file: any) {
-    const fileBlob = new Blob(file);
-    console.info(fileBlob);
+    const fileReader = new FileReader();
+
+    fileReader.onload = (e) => {
+      const text = e.target?.result;
+      console.info('TEXT ===================================> ', text);
+    };
+
+    fileReader.readAsText(file);
+
+    // const content = "File content to save";
+    // const element = document.createElement("a");
+    // const file = new Blob([content], {type: "text/plain"});
+    // element.href = URL.createObjectURL(file);
+    // element.download = "file.txt";
+    // element.click();
   }
 
   static createDownloader() {
-    const fileStream = fs.createWriteStream('');
+    // const fileStream = fs.createWriteStream('');
   }
 }
 
